756c89139c13136e3e462c221ace455a9e6c803e
lipestile <felipebalbino53@gmail.com>
2023-03-30 19:51:30 -0300

    :construction: atç

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import Rodape from "../components/Rodape";
 export default function Home() {
   return (
     <>
-      <Cabecalho></Cabecalho>
+      <Cabecalho/>
       <h1>Fucking world</h1>
       <h2>React</h2>
-      <Rodape></Rodape>
+      <Rodape/>
     </>
 
   )
