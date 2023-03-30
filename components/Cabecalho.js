2f4fe107e8026893fd2805095b7f400f97f1540c
lipestile <felipebalbino53@gmail.com>
2023-03-30 19:50:02 -0300

    🚧atualização 3/2023

---
+++ components/Cabecalho.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cabecalho = () => {
+  return (
+    <h1>Cabeçalho</h1>
+  )
+}
+
+export default Cabecalho
---
+++ pages/pagina2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pagina2 = () => {
+  return (
+    <div>pagina2</div>
+  )
+}
+
+export default pagina2
